8e9ec96e845f3035ce6fce58f47414cd243f55cc
ZlyukaExel <126321708+ZlyukaExel@users.noreply.github.com>
2024-10-17 19:00:13 +0300

    main com

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите x: ");
+        double x = sc.nextDouble();
+        System.out.print("Введите n: ");
+        int n = sc.nextInt();
+        System.out.print("Введите e: ");
+        double e = sc.nextDouble();
+
+        double sumOfN = 0, grThanE = 0, grThanEDivTen = 0, funcRes, prevVal = x;
+        for (int i = 1; i < n * 2; i+=2) {
+            double res = Math.pow(-1, i) * f(i, prevVal, x);
+
+            sumOfN += res;
+            if (Math.abs(res) > e / 10) {
+                grThanEDivTen += res;
+                if(Math.abs(res) > e)
+                    grThanE += res;
+            }
+            prevVal = res;
+        }
+
+        funcRes = Math.exp(x + Math.sqrt(x * x + 1));
+
+        System.out.println("\nСумма слагаемых = " + sumOfN +
+                "\nСумма абсолютных элементов больше e = " + grThanE +
+                "\nСумма абсолютных элементов больше e/10 = " + grThanEDivTen +
+                "\nЗначение функции = " + funcRes);
+    }
+
+    public static  double f(int n, double prev, double x){
+        return prev * n / (n + 1) * n * x * x / (n + 2);
+    }
+}
